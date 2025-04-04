5fb7df6ac822c33f19e5535cc058e44839d17008
nguyent11 <nguyen.tyler.n@gmail.com>
2025-04-04 17:07:09 -0500

    add file and camera menu actions

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,11 @@
 #include <QAction>
 #include <QMenuBar>
 #include <QStatusBar>
-#include <QVector>
-#include <QString>
 #include <QVBoxLayout>
 #include <QDebug>
+#include <QString>
 
 // Processing includes
-#include <usb.h>
 
 class MainWindow : public QMainWindow {
     public:
@@ -20,7 +18,7 @@ class MainWindow : public QMainWindow {
         setWindowTitle("");
 
         // Set the initial window size
-        resize(800, 600);
+        // resize(800, 600);
 
         // Create a central widget (optional, can use for main content)
         //QPushButton *button = new QPushButton("Hello, World!", this);
@@ -29,41 +27,40 @@ class MainWindow : public QMainWindow {
         // Create a menu bar
         QMenuBar *menuBar = this->menuBar();
 
-        // Create the "File" menu and add actions
+        // Create the "File" menu. Used to record and save to file.
         QMenu *fileMenu = menuBar->addMenu("File");
+	
 
-        statusBar()->showMessage("Ready");
+    	// Create "Camera" menu. Used to select camera
+	    QMenu *cameraMenu = menuBar->addMenu("Camera");
 
-        // Create the list of usb devices for the user to select from
-        QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-        QVector<QPushButton*> buttonVector;
-        QStringList usbDevices = {"Device_1", "Device_2", "Device_3"};
-        for (const QString &device : usbDevices) {
-            QPushButton *button = new QPushButton(device, this);
-            // Connect button clicked signal to a lambda or slot
-            connect(button, &QPushButton::clicked, [this, device]() {
-                qDebug() << "Button for " << device << " clicked.";
-            });
-            buttonVector.append(button);
-        }
-        // Add buttons from vector to layout
-        for (QPushButton *button : buttonVector) {
-            layout->addWidget(button);
-        }
+        createMenuActions(fileMenu, cameraMenu);
 
-    
-        centralWidget->setLayout(layout);
-        setCentralWidget(centralWidget);
+	    // Message at bottom of screen
+        statusBar()->showMessage("Status: Init");
+
         
+
+    }
+
+    void setStatus(QString statusmsg) {
+        this->statusBar()->showMessage(statusmsg);
     }
 
     private:
-    std::vector<std::string> grabUsbDevices() {
-        std::vector<std::string> deviceList;
+    void createMenuActions(QMenu* fileMenu, QMenu* cameraMenu) {
+        // File menu actions
+        QAction *recordAction = new QAction(tr("&Record to File"), this);
+        //recordAction->setShortcut(QKeySequence::SaveAs);
+        //connect(recordAction, &QAction::triggered, this, &MainWindow::saveAs);
+        fileMenu->addAction(recordAction);
 
-        deviceList.push_back("?");
-
-        return deviceList;
+        // Camera menu actions
+        QStringList devices = {"Device_1", "Device_2", "Device_3"};
+        for (int i = 0; i < devices.length(); i++) {
+            QAction *cameraAction = new QAction(devices[i], this);
+            cameraMenu->addAction(cameraAction);
+        }
     }
 };
 
@@ -75,10 +72,14 @@ int main(int argc, char** argv) {
     //button.show();
 
     MainWindow mainWindow;
+    mainWindow.showMaximized();
     mainWindow.show();
 
    // QPushButton button("Hello world!", &mainWindow);
    // button.show();
+
+
+    mainWindow.setStatus(QString("Status: Ready"));
 
     return app.exec();
 }
